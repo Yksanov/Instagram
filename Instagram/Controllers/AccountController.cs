e32e3b9db6593b8a10ac2f38c161de309670f597
Eldos Yksanov <eldosyksanov@gmail.com>
2024-11-14 15:03:20 +0600

    added new two actions: Login, Register and Logout

--- Instagram/Controllers/AccountController.cs
+++ Instagram/Controllers/AccountController.cs
@@ -2,6 +2,8 @@ using Instagram.Models;
 using Instagram.ViewModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace Instagram.Controllers;
 
@@ -22,5 +24,104 @@ public class AccountController : Controller
     public IActionResult Index(string returnUrl = "")
     {
         return View(new LoginViewModel { ReturnUrl = returnUrl });
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            Employees user = await _userManager.FindByEmailAsync(model.Email);
+
+            if (user == null)
+            {
+                user = await _userManager.FindByEmailAsync(model.Email);
+            }
+
+            SignInResult result = await _signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+
+            if (result.Succeeded)
+            {
+                if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+
+                return RedirectToAction("Index"); // надо изменить!!
+            }
+            
+            ModelState.AddModelError(string.Empty, "Неправильное логин или пароль");
+        }
+
+        return View(model);
+    }
+
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public async Task<IActionResult> Register(RegisterViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            string filename = $"avatar_{model.Email}{Path.GetExtension(model.Avatar.FileName)}";
+
+            if (model.Avatar != null && model.Avatar.Length > 0 && model.Avatar.ContentType.StartsWith("image/"))
+            {
+                string filePath = Path.Combine(_environment.WebRootPath, "avatars", filename);
+                Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await model.Avatar.CopyToAsync(stream);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("Avatar", "Изображение может быть только картинкой(image)");
+                return View(model);
+            }
+
+            Employees user = new Employees()
+            {
+                Email = model.Email,
+                UserName = model.UserName.ToLower(),
+                Name = model.Name,
+                Bio = model.Bio,
+                PhoneNumber = model.PhoneNumber,
+                Sex = model.Gender
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Account");  // надо изменить
+            }
+
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> LogOut()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index");
     }
 }
--- Instagram/obj/Debug/net8.0/Instagram.AssemblyInfo.cs
+++ Instagram/obj/Debug/net8.0/Instagram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Instagram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+175f667621b0d93963168bf14a374374cd12c162")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8cd7d8ba629541f1d3029e9d62d71dfd851f168")]
 [assembly: System.Reflection.AssemblyProductAttribute("Instagram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Instagram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
